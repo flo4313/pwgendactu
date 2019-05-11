f3104624973b5662e24d3e9f854508fadad0af2a
flo4313 <fsmague@gmail.com>
2019-05-11 11:09:48 +0200

    Ajout validators

---
+++ gendactu/app/Validators/ConnectUser.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class ConnectUser {
+  get rules () {
+    return {
+      'userId': 'required',
+      'password': 'required'
+    }
+  }
+
+  get messages(){
+    return{
+      'required': 'Veuillez remplir le champ ci-dessus',
+    }
+  }
+
+  async fails(error){
+    this.ctx.session.withErrors(error)
+    .flashAll();
+    return this.ctx.response.redirect('back');
+  }
+}
+
+module.exports = ConnectUser
