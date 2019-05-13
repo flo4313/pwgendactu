af761ddeea70e665c3d3199f3e1f9c7e0cab3c1c
flo4313 <fsmague@gmail.com>
2019-05-13 18:11:40 +0200

    Update view and routes

--- gendactu/start/routes.js
+++ gendactu/start/routes.js
@@ -18,8 +18,23 @@ const Route = use('Route')
 
 Route.on('/').render('index');
 Route.get('actu','TopicController.home');
+
 Route.on('actu').render('layouts.accueil');
 Route.on('/index').render('auth.login');
+
 Route.post('/index','UserController.login').validator('ConnectUser');
 
-Route.get('/topic/:id','TopicController.show')
+Route.get('/topics/create','ThemeController.all');
+Route.get('/topics/edit/:id','TopicController.edit');
+
+Route.get('/topics/delete/:id','TopicController.delete');
+Route.get('/topic/delete/image/:id','TopicController.deleteImage');
+
+Route.post('/topics/create','TopicController.create').validator('TopicCreate');
+Route.get('/topics/:id','TopicController.show');
+Route.put('/topics/edit/:id','TopicController.update').validator('TopicCreate');
+
+Route.group(()=>{
+    Route.get('gestion','TopicController.gestion');
+
+}).prefix('topics')
