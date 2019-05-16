f5bcc6ec06b7be0b49313f4c89e53d43a6adb3ed
flo4313 <fsmague@gmail.com>
2019-05-16 03:03:53 +0200

    Update topic

--- gendactu/app/Controllers/Http/TopicController.js
+++ gendactu/app/Controllers/Http/TopicController.js
@@ -1,19 +1,15 @@
 'use strict'
 
+const Database = use('Database')
 const Topic = use('App/Models/Topic')
 const Department = use('App/Models/Department')
 const Theme = use('App/Models/Theme')
 const Gendarmerie = use('App/Models/Gendarmerie')
+const Diriger = use('App/Models/Diriger')
 const Helpers = use('Helpers')
 
 
 class TopicController {
-    async home({view}){
-        const topics =  await Topic.all();
-        const departments = await Department.all();
-        const themes = await Theme.all();
-        return view.render('layouts.accueil',{topics : topics.toJSON(),departments : departments.toJSON(),themes : themes.toJSON()})
-    }
 
     async show({response,session,params,view}){
         const id = params.id;
@@ -83,20 +79,60 @@ class TopicController {
         return response.redirect('back');
     }
 
-    async gestion({view}){
+    async gestion({request,response}){
         const chefId = "00416873";
-        const gendarmeries = await Gendarmerie.query().where('chef',chefId).first();
+        const dirigers = await Diriger.query().where('chef',chefId).first();
+        const diriger = dirigers.toJSON();
+        const gendarmeries = await Gendarmerie.query().where('gendarmerieId',diriger.gendarmerie).first()
         const gendarmerie = gendarmeries.toJSON();
         const topics =  await Topic.query().where('author',gendarmerie.gendarmerieId).fetch();
-        return view.render('layouts.gestion',{topics : topics.toJSON(), gendarmerie:gendarmerie})
+        return response.json({
+            topics : topics,
+            gendarmerie : gendarmeries
+        })
     }
 
-    async delete({response,session,params}){
+
+    async getNbPages({params,request,response}){
+        var dept = params.id;
+        const topics =  await Database.raw('Select count(*) as nbTopics From topics t, gendarmeries g Where t.author = g.gendarmerieId and g.department = '+dept);
+        var topic = topics[0];
+        return response.json({
+            nbP : topic
+        })
+    }
+
+    async byDep({params,request,response}){
+        var res = 1;
+        var dept = params.department;
+        var page = params.page - 1;
+        const nbP =  await Database.raw('Select count(*) as nbTopics From topics t, gendarmeries g Where t.author = g.gendarmerieId and g.department = '+dept);
+        var nbT = nbP[0][0].nbTopics ;
+        var nbTA = nbT - (page*5);
+        if(nbTA > 5){
+            nbTA = 5 ;
+        }
+        const topics = await Database.raw("Select t.id, t.topicId, t.title, t.image, t.description, t.author, t.theme, t.created_at From topics t , gendarmeries g Where t.author = g.gendarmerieId and g.department = "+ dept+" Order by t.created_at Desc limit "+page+","+nbTA);
+        if(topics[0][0]== null){
+            res = null;
+        }
+        return response.json({    
+            topics : topics[0],
+            res : res
+        })
+    }
+
+    async delete({response,params}){
+        let resultat = "no remove";
+        console.log('enter');
         const topic = await Topic.query().where('topicId',params.id).first();
-        console.log(topic);
-        await  topic.delete();
-        session.flash({message: 'Le topic a été supprimé'});
-        return response.redirect('back');
+        if (topic){
+            resultat = "remove";
+            await  topic.delete();
+        }
+        return response.json({
+            valeur : resultat
+        })
     }
 
     async create({request, auth,response, session}){
